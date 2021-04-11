995503b42381c8f12a5b15815ad41b9bc15d2615
Marcos Paulo <marcos.p.mcruz@outlook.com>
2021-04-11 20:47:03 -0300

    url da criacao de topicos

--- forum/urls.py
+++ forum/urls.py
@@ -7,4 +7,5 @@ app_name = 'forum'
 urlpatterns = [
     path("", views.List.as_view(), name = 'list'),
     path("<int:pk>/", views.Detail.as_view(), name='detail'),
+    path("create/", views.Create.as_view(), name='novo-topico'),
 ]
