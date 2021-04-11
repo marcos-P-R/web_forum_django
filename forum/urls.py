ac7aff99cec584c11076ad06497b97390543252e
Marcos Paulo <marcos.p.mcruz@outlook.com>
2021-04-11 11:06:54 -0300

    criando as views com as urls que faz referencia das mesmas

--- forum/urls.py
+++ forum/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = 'forum'
+
 urlpatterns = [
-    path('test/', views.test, name='test'),
+    path("", views.List.as_view(), name = 'list'),
+    path("<int:pk>/", views.Detail.as_view(), name='detail'),
 ]
--- forum/views.py
+++ forum/views.py
@@ -2,6 +2,17 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+from django.views import generic
+
+from .models import Post
+
 # Create your views here.
-def test(request):
-    return HttpResponse("Testando se tudo esta funcionando")
+class List(generic.ListView):
+    model = Post
+    #template_name = "TEMPLATE_NAME"
+
+
+class Detail(generic.DetailView):
+    model = Post
+    #template_name = "TEMPLATE_NAME"
+
