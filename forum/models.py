21a6149810b5da0cd89f2ba491af0c29a3807d95
Marcos Paulo <marcos.p.mcruz@outlook.com>
2021-04-10 21:11:05 -0300

    Criando a classe model e adicionando o app no settings

--- forum/models.py
+++ forum/models.py
@@ -1,3 +1,8 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=150)
+    body_text = models.TextField()
+    name_author = models.ForeignKey(User, on_delete=models.CASCADE)
