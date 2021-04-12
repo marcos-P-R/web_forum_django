547ef26a2014f3bfdc0984ffea5a0249f7873e7c
Marcos Paulo <marcos.p.mcruz@outlook.com>
2021-04-12 16:01:51 -0300

    revertendo algumas funcionalidades que não tive como implementar

--- forum/form.py
+++ forum/form.py
@@ -1,12 +1,12 @@
 from django.forms import ModelForm
-from .models import Post, Answer
+from .models import Post #Answer
 
 class Insere_topico_form(ModelForm):
     class Meta:
         model = Post
         fields = ('title', 'body_text', 'name_author',)
 
-class Insere_resposta(ModelForm):
-    class Meta:
-        model = Answer
-        fields = ('text_answer', 'name',)
+#class Insere_resposta(ModelForm):
+#    class Meta:
+#        model = Answer
+#        fields = ('text_answer', 'name',)
